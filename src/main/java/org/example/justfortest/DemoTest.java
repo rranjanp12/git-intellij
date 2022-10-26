413755f8f4f302e556a48a0589ad21e05c6f8efa
Rashmi <rashmi.rrp@gmail.com>
2022-10-26 09:34:16 +0530

    adding 2nd file

---
+++ src/main/java/org/example/justfortest/DemoTest.java
@@ -0,0 +1,9 @@
+package org.example.justfortest;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class DemoTest {
+    void someThing(){
+        System.out.println("Lets write something...");
+    }
+}
---
+++ src/main/java/org/example/justfortest/DemoTest3.java
@@ -0,0 +1,4 @@
+package org.example.justfortest;
+
+public class DemoTest3 {
+}
