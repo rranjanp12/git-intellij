2be565907de9c32e571fc5b48e0d46a05d2462aa
Rashmi <rashmi.rrp@gmail.com>
2022-10-26 09:36:37 +0530

    adding 2nd file

--- src/main/java/org/example/justfortest/DemoTest3.java
+++ src/main/java/org/example/justfortest/DemoTest3.java
@@ -1,4 +1,7 @@
 package org.example.justfortest;
 
 public class DemoTest3 {
+    public static void main(String[] args) {
+        System.out.println("This code should b available in the repo");
+    }
 }
